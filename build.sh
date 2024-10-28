fc2f0f84d2c38fbcb61fe18855bc3bb6e729859a
Nania-X <mk258855@outlook.com>
2024-10-29 05:40:46 +0800

    更新 build.sh

--- build.sh
+++ build.sh
@@ -10,12 +10,8 @@ cat << EOF >> feeds.conf.default
 src-git diy1 https://github.com/xiaorouji/openwrt-passwall-packages.git;main
 src-git diy2 https://github.com/xiaorouji/openwrt-passwall.git;main
 src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main
-
-#!/MosDNS
 src-git moddns1 https://github.com/sbwml/luci-app-mosdns.git;v5-lua
 src-git mosdns2 https://github.com/sbwml/v2ray-geodata.git;v5-lua
-
-#!/iStore
 src-git istore https://github.com/linkease/istore-ui.git;main
 
 EOF
