ae65249106e222a01c8bd207644cb2c773302353
Nania-X <mk258855@outlook.com>
2024-10-29 05:16:26 +0800

    更新 build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ cat << EOF >> feeds.conf.default
 src-git diy1 https://github.com/xiaorouji/openwrt-passwall-packages.git;main
 src-git diy2 https://github.com/xiaorouji/openwrt-passwall.git;main
 src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main
+
+
+
 EOF
 rm target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-photonicat.dts
 cp $workdir/configs/rk3568-photonicat-usb30.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-photonicat.dts
